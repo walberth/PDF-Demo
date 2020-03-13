d26e9336c0ab24bcc5cabed21e51106be0aefc99
Walberth <w.felipe.gutierrez@gmail.com>
2020-03-13 13:04:44 -0500

    FIRST COMMIT

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1 {
+    class Program {
+        static void Main(string[] args)
+        {
+            var renderer = new IronPdf.HtmlToPdf();
+            renderer.RenderHtmlAsPdf("<h1>Html with CSS and Images</h1>").SaveAs("example.pdf");
+
+            var pdf = renderer.RenderHtmlAsPdf("<img src='test.jpg'>", @"C:\Users\walbe\Desktop\ConsoleApp1\");
+            pdf.SaveAs("html-with-assets.pdf");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
