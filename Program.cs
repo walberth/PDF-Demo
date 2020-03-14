564018d1fccb5a950ba8949737386de17ea3e065
Walberth <w.felipe.gutierrez@gmail.com>
2020-03-13 19:25:58 -0500

    cambios

--- Program.cs
+++ Program.cs
@@ -5,7 +5,8 @@ namespace ConsoleApp1 {
         static void Main(string[] args)
         {
             var renderer = new IronPdf.HtmlToPdf();
-            renderer.RenderHtmlAsPdf("<h1>Html with CSS and Images</h1>").SaveAs("example.pdf");
+            renderer.RenderHTMLFileAsPdfAsync(@"C:\Users\walbe\Desktop\ConsoleApp1\Demo.html").Result.SaveAs("demo.pdf");
+            //renderer.RenderHtmlAsPdf("<h1>Html with CSS and Images</h1>").SaveAs("example.pdf");
 
             var pdf = renderer.RenderHtmlAsPdf("<img src='test.jpg'>", @"C:\Users\walbe\Desktop\ConsoleApp1\");
             pdf.SaveAs("html-with-assets.pdf");
